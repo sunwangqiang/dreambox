aca96fc078efbc9eafc67665b0e77a0d00192b00
sun.wangqiang <sun.wangqiang@qq.com>
2017-03-25 19:34:39 +0800

    优化autosize指令

--- src/common/autosize.directive.ts
+++ src/common/autosize.directive.ts
@@ -1,26 +1,27 @@
-import {ElementRef, HostListener, Directive, OnInit} from '@angular/core';
+import { ElementRef, HostListener, Directive, OnInit, AfterViewChecked } from '@angular/core';
 
 @Directive({
   selector: 'ion-textarea[autosize]'
 })
 
-export class Autosize implements OnInit {
+export class Autosize implements AfterViewChecked {
+  textArea: any;
   @HostListener('input', ['$event.target'])
-  onInput(textArea:HTMLTextAreaElement):void {
+  onInput(textArea: HTMLTextAreaElement): void {
     this.adjust();
   }
 
-  constructor(public element:ElementRef) {
+  constructor(public element: ElementRef) {
   }
-
-  ngOnInit():void {
-    setTimeout(() => this.adjust(), 0);
+  ngAfterViewInit():void{
+    this.textArea = this.element.nativeElement.getElementsByTagName('textarea')[0];
   }
-
-  adjust():void {
-    let textArea = this.element.nativeElement.getElementsByTagName('textarea')[0];
-    textArea.style.overflow = 'hidden';
-    textArea.style.height = 'auto';
-    textArea.style.height = textArea.scrollHeight + "px";
+  ngAfterViewChecked(): void {
+    this.adjust();
+  }
+  adjust(): void {
+      this.textArea.style.overflow = 'hidden';
+      this.textArea.style.height = 'auto';
+      this.textArea.style.height = this.textArea.scrollHeight + "px";
   }
 }
