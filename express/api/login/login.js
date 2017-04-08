fc7370fc1c16aded6434d67b0e87b1abd265ff08
sun.wangqiang <sun.wangqiang@qq.com>
2017-04-09 07:31:57 +0800

    将登录相关的内容都集中到login.js中

--- express/api/login/login.js
+++ express/api/login/login.js
@@ -2,12 +2,19 @@ var debug = require('debug')('idream')
 var app = require('express')();
 var session = require(__dirname+'/../../lib/session.js');
 
+/**
+ * connect to db, check user info
+ * if user is valid, create session info
+ */
 function loginCheckUser(req, res, next)
 {
     debug("loginCheckUser, connect to mongodb check user");
     next();
 }
 
+/**
+ * insert user name to session
+ */
 function loginCheckResponse(req, res, next)
 {
     if(req.session){
@@ -17,6 +24,22 @@ function loginCheckResponse(req, res, next)
     }
     res.status(500).end("session service error");
 }
+/**
+ * check session info for all req of /api/xxx/....
+ */
+function sessionCheck(req, res, next)
+{
+    console.log(req.session);
+    if(!req.session){
+        return res.status(401).end("not auth");
+    }
+    if(!req.session.login){
+        return res.status(401).end("not login");
+    }
+    next();
+}
 
 app.use('/login', loginCheckUser, session, loginCheckResponse);
+app.use('/api', session, sessionCheck);
+
 module.exports = app;
