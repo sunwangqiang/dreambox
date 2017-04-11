30e1b4c775df7b8417eb15fd7b88ed74038df5cd
sun.wangqiang <sun.wangqiang@qq.com>
2017-04-12 00:30:34 +0800

    打通http到mongo数据库之间的通路

--- express/src/modules/useradmin.ts
+++ express/src/modules/useradmin.ts
@@ -1,17 +1,26 @@
 import * as debugModule from 'debug';
 const debug = debugModule('idream.UserAdmin')
 
+import { MongoClient, Db, InsertOneWriteOpResult } from 'mongodb'
+
 interface UserInfo{
     username:string;
     password:string;
 }
 
+const userInfoUrl:string = 'mongodb://localhost/userinfo';
+const userInfoCollection:string = 'userinfo';
+
 class UserAdmin{
     private static usrAdmin: UserAdmin;
     testUser:UserInfo = {username:"username", password:"password"};
+    db:Db;
 
     private constructor(){
-
+        MongoClient.connect(userInfoUrl).then((db:Db)=>{
+            console.log("mongodb connect ok");
+            this.db = db;
+        });
     }
     static getInstance():UserAdmin {
         if (!UserAdmin.usrAdmin) {
@@ -23,15 +32,34 @@ class UserAdmin{
     allocUser():Promise<UserInfo>{
         return Promise.resolve(this.testUser);
     }
+
     addUser(userInfo:UserInfo):Promise<boolean>{
         //save hash value to DB 
         debug("adduser ", userInfo);
-        return Promise.resolve(true);
+        return this.db.collection(userInfoCollection).insertOne(userInfo).then((ret:InsertOneWriteOpResult)=>{
+            if(ret.result.ok == 1){
+                return Promise.resolve(true);
+            }else{
+                return Promise.resolve(false);
+            }
+        })
     }
+    /**
+     * check whether userInfo is in db
+     * @param userInfo 
+     */
     checkUserInfo(userInfo:UserInfo):Promise<boolean>{
         //save hash value to DB 
         debug("checkUserInfo ", userInfo);
-        return Promise.resolve(true);
+        return this.db.collection(userInfoCollection).findOne(userInfo).then((doc)=>{
+            console.log(doc);
+            if(doc){
+                return Promise.resolve(true);
+            }else{
+                return Promise.resolve(false);
+            }
+            
+        })
     }
     checkUserName(userInfo:UserInfo):Promise<boolean>{
         //save hash value to DB 
